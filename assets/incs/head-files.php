042a652acd315ad9dc47d31844a1ecf86c95f567
Kosi Eric <itskosieric@gmail.com>
2019-04-03 13:41:55 +0100

    Switched to es5

--- assets/incs/head-files.php
+++ assets/incs/head-files.php
@@ -4,20 +4,15 @@ $cdn_required = <<<CDN_INCLUDES
      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-<script crossorigin src='https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.js' type="text/javascript"></script>
-
-<script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
-<script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
-<script crossorigin src = 'https://cdnjs.cloudflare.com/ajax/libs/redux/4.0.1/redux.min.js'></script>
-<script crossorigin src='https://cdnjs.cloudflare.com/ajax/libs/react-redux/6.0.1/react-redux.min.js'></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
 
 CDN_INCLUDES;
 $materialize_css = array("materialize.min.css");
 $stylesheets = array("main.css"  , "footer.css");
-$required =  array("jquery.min.js" ,  "materialize.min.js" , "babel.min.js"  , "react.development.js" , "react-dom.development.js" , "redux.min.js" , "react-redux.min.js");
-
+$required =  array("jquery.min.js" ,  "materialize.min.js");
+$required2 = array("babel.min.js"  , "react.development.js" , "react-dom.development.js" , "redux.min.js" , "react-redux.min.js");
 echo $functions->is_production_mode() ? $cdn_required : $functions->printAssets($materialize_css , "css" , false).$functions->printAssets($required);
 echo $functions->printAssets($stylesheets , "css" , false)."\n";
+echo $functions->printAssets($required2 , "JavaScript");
 
 ?>
