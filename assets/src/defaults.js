d53134cfc10dd621af19662c470b4a19fec42815
Kosi Eric <itskosieric@gmail.com>
2019-04-27 12:52:47 +0100

    Evrything working fine

--- assets/src/defaults.js
+++ assets/src/defaults.js
@@ -129,9 +129,8 @@ Pace.on('done' , function (e) {
 class Defaults {
 
     getFileLocation(filename)  {
-        //return this.isProductionMode ? '/' + this.siteNameLowercase  + filename : filename;
-
-    return this.isProductionMode ? "https://rawcdn.githack.com/movybe/web-production/212324f03701737a9a30b76c3c0754577b72a4dc\n/" : filename;
+        return this.isProductionMode ? '/' + this.siteNameLowercase  + filename : filename;
+    //return this.isProductionMode ? "https://rawcdn.githack.com/movybe/web-production/212324f03701737a9a30b76c3c0754577b72a4dc\n/" : filename;
     };
 
 
