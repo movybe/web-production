50e6f03ada65336799fb92244aa51c280aa2e7f1
Kosi Eric <itskosieric@gmail.com>
2019-05-09 13:19:28 +0100

    working on site performance

--- assets/incs/head-files.php
+++ assets/incs/head-files.php
@@ -26,6 +26,7 @@ $pace_css = array('pace.css');
 echo $functions->printAssets($pace_js , 'javascript' , true , null , "id = 'pace-js'");
 $cdn_required = <<<CDN_INCLUDES
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
 <script src='https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.7/es5-shim.min.js'></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
@@ -35,13 +36,14 @@ $cdn_required = <<<CDN_INCLUDES
 <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
 <script crossorigin src = 'https://cdnjs.cloudflare.com/ajax/libs/redux/4.0.1/redux.min.js'></script>
 <script crossorigin src='https://cdnjs.cloudflare.com/ajax/libs/react-redux/6.0.1/react-redux.min.js'></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
 CDN_INCLUDES;
 $materialize_css = array("materialize.min.css");
 $stylesheets = array("main.css"  , "footer.css");
 $required = array("jquery.min.js" ,  "materialize.min.js" , "babel.min.js"  , "react.development.js" , "react-dom.development.js" , "redux.min.js" , "react-redux.min.js");
-echo $functions->is_production_mode() ? $cdn_required : $functions->printAssets($materialize_css , "css" , false , null).$functions->printAssets($required)."\n";
-echo $functions->printAssets($stylesheets , "css" , false)."\n";
 echo $functions->printAssets($pace_css , 'css' , false , null , "title = 'pace-css' id ='pace-css'");
+echo $functions->printAssets($stylesheets , "css" , false)."\n";
+echo $functions->is_production_mode() ? $cdn_required : $functions->printAssets($materialize_css , "css" , false , null).$functions->printAssets($required)."\n";
+
+
 
 ?>
