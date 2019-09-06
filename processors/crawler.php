eed345bbdd5763d2c58be2bc0a85fffaadd471d9
Kosi Eric <itskosieric@gmail.com>
2019-09-06 01:57:10 +0100

    changed my user-agent to google Bot

--- processors/crawler.php
+++ processors/crawler.php
@@ -135,7 +135,7 @@
 //
 // ############################################################################
 
-//require_once '../config/functions.php';
+require_once '../config/functions.php';
 
 ini_set('user_agent', "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36");
 
