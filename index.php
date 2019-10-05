dfd7940941387f8800ebf5932266d93de0453aa7
Kosi Eric <itskosieric@gmail.com>
2019-10-05 02:13:31 +0100

    cached some images

--- index.php
+++ index.php
@@ -15,8 +15,8 @@ if($functions->data_saving_mode_is_enabled())
     <meta content="<?php echo $website_details->WebsiteSubject;?>" property="og:title">
     <meta content="<?php echo $website_details->PageDescription; ?>" property="og:description">
     <meta name="description" content="<?php echo $website_details->PageDescription; ?>" />
-    <link rel="manifest" href="/manifest.json?expires=January 01 2022 01:00:00 AM" />
-    <link rel="manifest" href="/manifest.webmanifest?expires=January 01 2022 01:00:00 AM" />
+    <link rel="manifest" href="/manifest.json?last_modified=October 05 2019 01:00:00 AM" />
+    <link rel="manifest" href="/manifest.webmanifest?last_modified=October 05 2019 01:00:00 AM" />
     <meta name="theme-color" content="#2299dd">
     <meta name="msapplication-TileColor" content="#2299dd">
     <meta name="msapplication-navbutton-color" content="#2299dd">
@@ -45,7 +45,7 @@ if($functions->data_saving_mode_is_enabled())
         <div class="container">
             <br><br>
             <h1 class="header center pink-text text-lighten-3">
-                <img data-src="<?php echo $website_details->IMG_FOLDER.strtolower($website_details->SiteName);?>-png.png" class="responsive-img brand-logo logo-image lazyload" width="220" height="81" alt = "<?php echo $website_details->SiteName." logo"?>" /></h1>
+                <img data-src="<?php echo $website_details->IMG_FOLDER.strtolower($website_details->SiteName);?>-png.png?last_modified=October 05 2019 01:00:00 AM" class="responsive-img brand-logo logo-image lazyload" width="220" height="81" alt = "<?php echo $website_details->SiteName." logo"?>" /></h1>
             <div class="row center">
                 <h5 class="header col s12 light grey-text text-lighten-1"><?php echo $website_details->PageDescription; ?></h5>
             </div>
