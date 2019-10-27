86655ebf5da1caedb98a7e59d3cf0e32a6398e0f
Kosi Eric <itskosieric@gmail.com>
2019-10-27 16:47:58 +0100

    working on proxy

--- processors/crawler.php
+++ processors/crawler.php
@@ -144,7 +144,7 @@ function is_production_mode () : bool
 }
 
 $url = isset($_GET['url']) ? $_GET['url'] : $_POST['url'];
-
+$proxy = 'http://'.$_SERVER['REMOTE_ADDR'].':22222';
 $url = strtolower($url);
 
 $user_agent_strings =
@@ -224,6 +224,7 @@ if ( !$url ) {
     curl_setopt($ch, CURLOPT_ENCODING,  '');
     curl_setopt( $ch, CURLOPT_HEADER, false);
     curl_setopt( $ch, CURLOPT_HTTPHEADER, array("REMOTE_ADDR: $ip", "HTTP_X_FORWARDED_FOR: $ip"));
+    if(!is_production_mode())curl_setopt($ch, CURLOPT_PROXY, $proxy);
     if (defined('CURLOPT_IPRESOLVE') && defined('CURL_IPRESOLVE_V4')){
         curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
     }
